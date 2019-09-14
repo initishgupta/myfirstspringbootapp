d73e9803590cc84686b5ea571a604c0ebf48d468
Nish <initishgupta@gmail.com>
2019-09-14 14:14:30 -0400

    Setting Spring boot application Properties. These properties can be used across different spring modules like core, web, database etc. Some examples listed in application.properties with URL to the references

--- src/main/java/com/luv2code/springboot/demo/myfirstspringbootapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/myfirstspringbootapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.myfirstspringbootapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,8 +31,18 @@ public class FunRestController {
     return toReturn;
   }
 
+//inject custom properties for: coach.name and team.name
+  @Value("${coach.name}")
+  private String coachName;
 
+  @Value("${team.name}")
+  private String teamName;
 
+  //expose new endpoint for "teaminfo"
 
+  @GetMapping("/teaminfo")
+  public String getTeamInfo() {
+    return "Coach: " + coachName + " Team: " + teamName;
+  }
 
 }
